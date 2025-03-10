1b9c9a9c6618a2a86331c171ba2f0e13ebc5b07b
NThuesen <130403420+NThuesen@users.noreply.github.com>
2025-03-10 11:00:15 -0300

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -1,39 +1,50 @@
-#include "hardware/gpio.h"
-#include "pico/stdlib.h"
+#include <hardware/gpio.h>
+#include <pico/stdlib.h>
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;
+volatile bool led_state = false;
+volatile uint64_t press_start_time = 0;
+volatile bool btn_pressed = false;
+
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 1;
-
-    } else if (events == 0x8) { // rise edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 0;
+    if (gpio == BTN_PIN_R) {
+        if (events == 0x4) {
+            press_start_time = to_ms_since_boot(get_absolute_time());
+            btn_pressed = true;
+        } 
+        else if (events == 0x8) { 
+            uint64_t press_duration = to_ms_since_boot(get_absolute_time()) - press_start_time;
+            btn_pressed = false;
+            if (press_duration >= 500) {
+                led_state = !led_state;
+            }
+        }
     }
 }
 
 int main() {
     stdio_init_all();
 
+  
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
-    gpio_set_irq_enabled_with_callback(
-        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
+  
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
     while (true) {
+    
+        gpio_put(LED_PIN_R, led_state);
 
-        if (flag_f_r) {
-        }
+        sleep_ms(10); 
     }
 }
